91da663eec1ee6a27e2a5930d9880d50a123bd73
Thales Macêdo <thalesmacedo1@live.com>
2022-03-31 20:09:16 -0300

    fix: corrected api verbs

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ const tasksController = new TasksController();
 routes.get('/tasks', tasksController.list);
 routes.post('/tasks', tasksController.create);
 routes.get('/tasks/:id', tasksController.show);
-routes.get('/tasks/:id', tasksController.update);
-routes.get('/tasks/:id', tasksController.delete);
+routes.put('/tasks/:id', tasksController.update);
+routes.delete('/tasks/:id', tasksController.delete);
 
 export default routes;
