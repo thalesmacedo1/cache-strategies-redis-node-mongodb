820a13be1adf2c138cafd30b1d924d134a936d88
Thales Macêdo <thalesmacedo1@live.com>
2022-03-31 19:50:55 -0300

    Listing total data and unified data

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -1,6 +1,15 @@
 import Task from '../models/Task'
 
 export default class TasksController {
+    async list(request, response) {
+        try {
+            const data = await Task.find({});
+            return response.status(200).json({ data });
+        } catch (error) {
+            return response.status(200).json({ error });
+        }
+    }
+
     async create(request, response) {
         try {
             const task = await Task.create(request.body);
