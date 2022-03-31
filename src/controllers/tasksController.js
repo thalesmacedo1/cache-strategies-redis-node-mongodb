18094630345c3b593e0b4119435aa0f2ff682654
Thales Macêdo <thalesmacedo1@live.com>
2022-03-31 19:50:55 -0300

    Listing total data and unified data

--- src/controllers/tasksController.js
+++ src/controllers/tasksController.js
@@ -1,12 +1,33 @@
 import Task from '../models/Task'
 
 export default class TasksController {
+    async list(request, response) {
+        try {
+            const data = await Task.find({});
+            return response.status(200).json({ data });
+        } catch (error) {
+            return response.status(200).json({ error });
+        }
+    }
+
     async create(request, response) {
         try {
             const task = await Task.create(request.body);
             return response.status(201).json({ task });
         } catch (error) {
             return response.status(500).json({ error });
+        }
+    }
+
+    async show(request, response) {
+        try {
+            const { id } = request.params;
+            if (!id) return response.status(400).json({ error: 'Invalid parameter'});
+
+            const data = await Task.findById(id);
+            return response.status(200).json({ data });
+        } catch (error) {
+            return response.status(200).json({ error });
         }
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,8 @@ const routes = Router();
 
 const tasksController = new TasksController();
 
-// routes.get('/tasks', tasksController.index);
-// routes.get('/tasks/:id', tasksController.index);
+routes.get('/tasks', tasksController.list);
 routes.post('/tasks', tasksController.create);
+routes.get('/tasks/:id', tasksController.show);
 
 export default routes;
